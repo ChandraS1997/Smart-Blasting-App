b7b41b74ed44e3329a158268b2b0ddaa8b8dbc5d
bd108 <vedgaurbdappmaniac99@gmail.com>
2023-06-14 18:03:17 +0530

    In Hole delay and Hole Delay Update in UI and API in Blades and Bims

--- app/src/main/java/com/smart_blasting_drilling/android/ui/adapter/MapHolePoint3dAdapter.java
+++ app/src/main/java/com/smart_blasting_drilling/android/ui/adapter/MapHolePoint3dAdapter.java
@@ -9,10 +9,14 @@ import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.google.gson.reflect.TypeToken;
 import com.smart_blasting_drilling.android.R;
 import com.smart_blasting_drilling.android.api.apis.response.hole_tables.AllTablesData;
 import com.smart_blasting_drilling.android.api.apis.response.hole_tables.Table1Item;
+import com.smart_blasting_drilling.android.api.apis.response.table_3d_models.Response3DTable2DataModel;
 import com.smart_blasting_drilling.android.app.BaseRecyclerAdapter;
 import com.smart_blasting_drilling.android.databinding.MapHoleColunmItemBinding;
 import com.smart_blasting_drilling.android.helper.Constants;
@@ -24,6 +28,7 @@ import com.smart_blasting_drilling.android.ui.activity.HoleDetailActivity;
 import com.smart_blasting_drilling.android.ui.models.MapHole3DDataModel;
 
 import java.lang.reflect.Type;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MapHolePoint3dAdapter extends BaseRecyclerAdapter {
@@ -80,16 +85,18 @@ public class MapHolePoint3dAdapter extends BaseRecyclerAdapter {
             String patternType = "Staggered";
 
             try {
+
                 if (!Constants.isListEmpty(appDatabase.projectHoleDetailRowColDao().getAllBladesProjectList())) {
                     if (appDatabase.projectHoleDetailRowColDao().getAllBladesProjectList().get(0) != null) {
-                        Type teamList = new TypeToken<List<Table1Item>>() {
-                        }.getType();
-                        AllTablesData tablesData = new Gson().fromJson(appDatabase.projectHoleDetailRowColDao().getAllBladesProject(((HoleDetail3DModelActivity) context).bladesRetrieveData.get(0).getDesignId()).getProjectHole(), AllTablesData.class);
-                        List<Table1Item> drillPatternDataItemList = tablesData.getTable1();
-                        if (!Constants.isListEmpty(drillPatternDataItemList)) {
-                            String[] drillPatternDataItem = new String[drillPatternDataItemList.size()];
-                            for (int i = 0; i < drillPatternDataItemList.size(); i++) {
-                                drillPatternDataItem[i] = drillPatternDataItemList.get(i).getPatternType();
+                        JsonArray array = new Gson().fromJson(new Gson().fromJson(new Gson().fromJson(appDatabase.projectHoleDetailRowColDao().getAllBladesProject(((HoleDetail3DModelActivity) context).bladesRetrieveData.get(0).getDesignId()).getProjectHole(), JsonObject.class).get(Constants._3D_TBALE_NAME).getAsJsonPrimitive(), String.class), JsonArray.class);
+                        List<Response3DTable2DataModel> holeDetailDataList = new ArrayList<>();
+                        for (JsonElement e : new Gson().fromJson(new Gson().fromJson(array.get(1), String.class), JsonArray.class)) {
+                            holeDetailDataList.add(new Gson().fromJson(e, Response3DTable2DataModel.class));
+                        }
+                        if (!Constants.isListEmpty(holeDetailDataList)) {
+                            String[] drillPatternDataItem = new String[holeDetailDataList.size()];
+                            for (int i = 0; i < holeDetailDataList.size(); i++) {
+                                drillPatternDataItem[i] = holeDetailDataList.get(i).getPatternType();
                             }
                             patternType = drillPatternDataItem[0];
                         }
