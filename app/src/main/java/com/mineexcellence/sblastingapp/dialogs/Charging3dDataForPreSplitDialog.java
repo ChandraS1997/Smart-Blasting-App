1249b600d61b61d8af6258f75d8a10c313f08bfa
vedprakashbiwal <vedgaurbdappmaniac99@gmail.com>
2023-09-30 10:29:35 +0530

    Charging For Pre Split is completed

--- app/src/main/java/com/mineexcellence/sblastingapp/dialogs/Charging3dDataForPreSplitDialog.java
+++ app/src/main/java/com/mineexcellence/sblastingapp/dialogs/Charging3dDataForPreSplitDialog.java
@@ -16,13 +16,24 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.reflect.TypeToken;
 import com.mineexcellence.sblastingapp.R;
 import com.mineexcellence.sblastingapp.api.apis.response.table_3d_models.ChargeTypeArrayItem;
 import com.mineexcellence.sblastingapp.api.apis.response.table_3d_models.Response3DTable16PilotDataModel;
 import com.mineexcellence.sblastingapp.api.apis.response.table_3d_models.pre_spilit_table.HoleDetailItem;
+import com.mineexcellence.sblastingapp.api.apis.response.table_3d_models.pre_spilit_table.Response3DTable18PreSpilitDataModel;
+import com.mineexcellence.sblastingapp.app.AppDelegate;
 import com.mineexcellence.sblastingapp.databinding.DialogChargingDataViewBinding;
 import com.mineexcellence.sblastingapp.helper.Constants;
 import com.mineexcellence.sblastingapp.room_database.dao_interfaces.ProjectHoleDetailRowColDao;
+import com.mineexcellence.sblastingapp.room_database.dao_interfaces.UpdateProjectBladesDao;
+import com.mineexcellence.sblastingapp.room_database.entities.ProjectHoleDetailRowColEntity;
+import com.mineexcellence.sblastingapp.room_database.entities.UpdateProjectBladesEntity;
 import com.mineexcellence.sblastingapp.ui.activity.BaseActivity;
 import com.mineexcellence.sblastingapp.ui.activity.HoleDetail3DModelActivity;
 import com.mineexcellence.sblastingapp.ui.adapter.Charging3dDataListAdapter;
@@ -44,6 +55,7 @@ public class Charging3dDataForPreSplitDialog extends BaseDialogFragment {
     Charging3dDataListAdapter adapter;
 
     HoleDetailItem allTablesData;
+    List<Response3DTable18PreSpilitDataModel> preSpilitDataModelList;
 
     public Charging3dDataForPreSplitDialog() {
         _self = this;
@@ -143,7 +155,106 @@ public class Charging3dDataForPreSplitDialog extends BaseDialogFragment {
             allTablesData.setTotalCharge(String.valueOf(totalCharge));
             allTablesData.setChargeLength(String.valueOf(chargeLen));
 
-            ((HoleDetail3DModelActivity) mContext).updateEditedDataForPreSplitIntoDb(allTablesData, true);
+            UpdateProjectBladesEntity bladesEntity = new UpdateProjectBladesEntity();
+            UpdateProjectBladesDao updateProjectBladesDao = appDatabase.updateProjectBladesDao();
+
+            bladesEntity.setData(new Gson().toJson(allTablesData));
+//            bladesEntity.setHoleId(Integer.parseInt(String.valueOf(allTablesData.getHoleNo())));
+//            bladesEntity.setRowId(Integer.parseInt(String.valueOf(allTablesData.getRowNo())));
+            bladesEntity.setDesignId(String.valueOf(allTablesData.getHoleId()));
+
+            if (!updateProjectBladesDao.isExistProject(String.valueOf(allTablesData.getHoleId()))) {
+                updateProjectBladesDao.insertProject(bladesEntity);
+            } else {
+                updateProjectBladesDao.updateProject(bladesEntity);
+            }
+
+//            JsonElement element = AppDelegate.getInstance().getHole3DDataElement();
+            JsonElement element = new Gson().fromJson(entity.getAllBladesProject(allTablesData.getDesignId()).getProjectHole(), JsonElement.class);
+            List<Response3DTable16PilotDataModel> allTablesDataChild = new ArrayList<>();
+            if (element != null) {
+                // GetAll3DDesignInfoResult for test
+                JsonArray array = new Gson().fromJson(new Gson().fromJson(((JsonObject) element).get(Constants._3D_TBALE_NAME).getAsJsonPrimitive(), String.class), JsonArray.class);
+                List<Response3DTable18PreSpilitDataModel> holeDetailDataList = new ArrayList<>();
+                JsonArray jsonArray = new JsonArray();
+                if (array.get(18) instanceof JsonArray) {
+                    jsonArray = new Gson().fromJson(array.get(18), JsonArray.class);
+                } else {
+                    jsonArray = new Gson().fromJson(new Gson().fromJson(array.get(18), String.class), JsonArray.class);
+                }
+                for (JsonElement e : jsonArray) {
+                    Response3DTable18PreSpilitDataModel model = new Gson().fromJson(e, Response3DTable18PreSpilitDataModel.class);
+                    model.setHoleDetailStr(model.getHoleDetailStr());
+                    model.setHolePointsStr(model.getHolePointsStr());
+                    model.setLinePointsStr(model.getLinePointsStr());
+                    holeDetailDataList.add(model);
+                }
+                if (!Constants.isListEmpty(holeDetailDataList)) {
+                    for (int ind = 0; ind < holeDetailDataList.size(); ind++) {
+                        Response3DTable18PreSpilitDataModel model = holeDetailDataList.get(ind);
+                        List<HoleDetailItem> itemList = new Gson().fromJson(holeDetailDataList.get(ind).getHoleDetailStr(), new TypeToken<List<HoleDetailItem>>() {
+                        }.getType());
+                        for (int i = 0; i < itemList.size(); i++) {
+                            HoleDetailItem item = itemList.get(i);
+                            if (item.getHoleId().equals(allTablesData.getHoleId())) {
+                                itemList.set(i, allTablesData);
+                            }
+                        }
+                        model.setHoleDetailStr(new Gson().toJson(itemList));
+                        holeDetailDataList.set(ind, model);
+                    }
+
+                    array.set(18, new Gson().fromJson(new Gson().toJson(holeDetailDataList), JsonElement.class));
+                    JsonObject jsonObject = new JsonObject();
+                    JsonPrimitive primitive = new JsonPrimitive(new Gson().toJson(array));
+                    jsonObject.add(Constants._3D_TBALE_NAME, primitive);
+                    preSpilitDataModelList = holeDetailDataList;
+
+                    if (!entity.isExistProject(String.valueOf(allTablesData.getDesignId()))) {
+                        entity.insertProject(new ProjectHoleDetailRowColEntity(String.valueOf(allTablesData.getDesignId()), true, new Gson().toJson(jsonObject)));
+                    } else {
+                        entity.updateProject(String.valueOf(allTablesData.getDesignId()), new Gson().toJson(jsonObject));
+                    }
+
+                }
+            }
+
+            try {
+                ProjectHoleDetailRowColDao dao = appDatabase.projectHoleDetailRowColDao();
+                ProjectHoleDetailRowColEntity colEntity = dao.getAllBladesProject(allTablesData.getDesignId());
+
+                JsonArray array = new Gson().fromJson(new Gson().fromJson(((JsonObject) new Gson().fromJson(colEntity.getProjectHole(), JsonElement.class)).get(Constants._3D_TBALE_NAME).getAsJsonPrimitive(), String.class), JsonArray.class);
+                List<Response3DTable18PreSpilitDataModel> holeDetailDataList = new ArrayList<>();
+                JsonArray jsonArray = new JsonArray();
+                if (array.get(16) instanceof JsonArray) {
+                    jsonArray = new Gson().fromJson(array.get(18), JsonArray.class);
+                } else {
+                    jsonArray = new Gson().fromJson(new Gson().fromJson(array.get(18), String.class), JsonArray.class);
+                }
+                for (JsonElement e : jsonArray) {
+                    holeDetailDataList.add(new Gson().fromJson(e, Response3DTable18PreSpilitDataModel.class));
+                }
+
+                if (((HoleDetail3DModelActivity) mContext).rowItemDetail != null)
+                    ((HoleDetail3DModelActivity) mContext).rowItemDetail.setRowOfTableForPreSplit(((HoleDetail3DModelActivity) mContext).rowPageVal, holeDetailDataList, false);
+
+                if (((HoleDetail3DModelActivity) mContext).mapViewDataUpdateLiveData != null)
+                    ((HoleDetail3DModelActivity) mContext).mapViewDataUpdateLiveData.setValue(true);
+
+                if (Constants.holeBgListener != null)
+                    Constants.holeBgListener.setBackgroundRefresh();
+                if (Constants.hole3DBgListener != null) {
+                    Constants.hole3DBgListener.setBackgroundRefresh();
+                }
+
+                AppDelegate.getInstance().setPreSpilitDataModelList(holeDetailDataList);
+                ((HoleDetail3DModelActivity) mContext).preSplitTableData = holeDetailDataList;
+                if (((HoleDetail3DModelActivity) mContext).mapPilotCallBackListener != null)
+                    ((HoleDetail3DModelActivity) mContext).mapPilotCallBackListener.saveAndCloseHoleDetailForPilotCallBack();
+
+            } catch (Exception e) {
+                e.getLocalizedMessage();
+            }
 
             showToast(mContext.getString(R.string.charging_added_msg));
             dismiss();
